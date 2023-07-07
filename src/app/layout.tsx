ab8be58da44330276835eae167b68149dd9619df
chihiros <szrkw2ui2a@gmail.com>
2023-07-08 00:31:52 +0900

    add: Providerの追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Provider from '@/app/provider';
 import './global.css';
 
 export const metadata: Metadata = {
@@ -9,7 +10,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <body className={``}>{children}</body>
+      <body className={``}>
+        <Provider>
+          {children}
+        </Provider>
+      </body>
     </html>
   )
 }
