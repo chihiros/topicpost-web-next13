08eeec3b2fde02d2eebef5fee79e3bf28df51968
chihiros <szrkw2ui2a@gmail.com>
2023-07-08 00:21:24 +0900

    add: cssの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,52 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>Hello World!!</div>
+    <div className="border-2 border-gray-200 border-dashed rounded-lg">
+      <div className="grid grid-cols-3 gap-4 mb-4">
+        <div className="flex items-center justify-center h-24 rounded bg-gray-50">
+          <p className="text-2xl text-gray-400">+</p>
+        </div>
+        <div className="flex items-center justify-center h-24 rounded bg-gray-50">
+          <p className="text-2xl text-gray-400">+</p>
+        </div>
+        <div className="flex items-center justify-center h-24 rounded bg-gray-50">
+          <p className="text-2xl text-gray-400">+</p>
+        </div>
+      </div>
+      <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50">
+        <p className="text-2xl text-gray-400">+</p>
+      </div>
+      <div className="grid grid-cols-2 gap-4 mb-4">
+        <div className="flex items-center justify-center rounded bg-gray-50 h-28">
+          <p className="text-2xl text-gray-400">+</p>
+        </div>
+        <div className="flex items-center justify-center rounded bg-gray-50 h-28">
+          <p className="text-2xl text-gray-400">+</p>
+        </div>
+        <div className="flex items-center justify-center rounded bg-gray-50 h-28">
+          <p className="text-2xl text-gray-400">+</p>
+        </div>
+        <div className="flex items-center justify-center rounded bg-gray-50 h-28">
+          <p className="text-2xl text-gray-400">+</p>
+        </div>
+      </div>
+      <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50">
+        <p className="text-2xl text-gray-400">+</p>
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="flex items-center justify-center rounded bg-gray-50 h-28">
+          <p className="text-2xl text-gray-400">+</p>
+        </div>
+        <div className="flex items-center justify-center rounded bg-gray-50 h-28">
+          <p className="text-2xl text-gray-400">+</p>
+        </div>
+        <div className="flex items-center justify-center rounded bg-gray-50 h-28">
+          <p className="text-2xl text-gray-400">+</p>
+        </div>
+        <div className="flex items-center justify-center rounded bg-gray-50 h-28">
+          <p className="text-2xl text-gray-400">+</p>
+        </div>
+      </div>
+    </div>
   )
 }
