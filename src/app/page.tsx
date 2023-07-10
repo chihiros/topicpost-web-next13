86898bb8927e2223a054ebb65e22da3e35c3ef4c
chihiros <szrkw2ui2a@gmail.com>
2023-07-10 23:00:22 +0900

    add: sampleを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import { Button, SubmitButton } from '@/components/atoms/Button';
+// import ArrowRight from '@/components/atoms/Icon/Arrow';
+import { Text, Textarea, Annotation } from '@/components/atoms/Input';
 
 export default function Home() {
   return (
-    <></>
+    <>
+      {/* <Button>aaaaa</Button> */}
+      <SubmitButton>aaaa</SubmitButton>
+      {/* <ArrowRight /> */}
+      {/* <Text
+        // type="text"
+        id="text"
+        type='text'
+        className='w-96'
+      />
+      <Textarea
+        id="textarea"
+        className='w-96'
+      />
+      <Annotation>aaaa</Annotation> */}
+    </>
   )
 }
