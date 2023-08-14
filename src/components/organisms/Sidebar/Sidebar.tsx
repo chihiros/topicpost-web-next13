529bc2d4b92195a2ff6041eac0047a33fcad54a1
chihiros <szrkw2ui2a@gmail.com>
2023-08-14 14:10:12 +0900

    add: Sidebarのコンポーネントを追加

---
+++ src/components/organisms/Sidebar/Sidebar.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Sidebar = () => {
+  // これする意味ないかも
+  const SidebarStyle = "transition-transform -translate-x-full lg:translate-x-0";
+
+  return (
+    <aside
+      id="logo-sidebar"
+      className={`fixed top-0 left-0 w-64 h-screen shadow ${SidebarStyle}`}
+      aria-label="Sidebar"
+    >
+      <div className="h-full py-4 overflow-y-auto bg-gray-50">
+        <Link href="/" className="flex justify-center pt-2 pb-4">
+          <span className="text-3xl font-semibold hover:text-gray-400">TopicPost</span>
+        </Link>
+
+        {/* {isLoggedIn ? <SidebarLoggedIn /> : <SidebarLogin />} */}
+
+        {/* {sidebar.map((label, index) => (
+          <SidebarLabel
+            key={index}
+            icon={label.icon}
+            label={label.label}
+            link={label.link}
+          />
+        ))} */}
+        <div className="mt-4">
+          <button
+            // onClick={handleLogout}
+            className="bg-slate-300 hover:bg-slate-700 text-white text-base font-bold py-2 px-4 rounded"
+          >
+            ログアウト
+          </button>
+        </div>
+      </div>
+    </aside>
+  )
+}
+
+export default Sidebar
