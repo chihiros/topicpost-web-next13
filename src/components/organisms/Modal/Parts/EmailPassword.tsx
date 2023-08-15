f4d51e5511c1cdcaf8980e4fbe0af86aa30291f0
chihiros <szrkw2ui2a@gmail.com>
2023-08-16 02:29:31 +0900

    fix: Inputで入力された内容を使えるように修正

--- src/components/organisms/Modal/Parts/EmailPassword.tsx
+++ src/components/organisms/Modal/Parts/EmailPassword.tsx
@@ -1,90 +1,88 @@
 'use client';
 import React, { useState } from 'react';
-// import { Link } from "react-router-dom";
 import Link from 'next/link';
-import { Text } from "../../../atoms/Input";
-import Label from '../../../atoms/Label';
 
-import { SubmitButton } from "../../../atoms/Button";
+import { Text } from '@/components/atoms/Input';
+import Label from '@/components/atoms/Label';
 
-// import Toast from "../../../../utils/Toast";
-// import { getErrorMessage } from "../../../../utils/ErrorMessage";
-// import { supabaseClient } from "../../../../utils/supabase";
+import { SubmitButton } from '@/components/atoms/Button';
+
+import toast from "@/utils/Toast";
+import { getErrorMessage } from "@/utils/ErrorMessage";
+import { supabaseClient } from "@/utils/supabase";
 // import { useHistory } from 'react-router-dom';
 // import { useAuthContext } from '../../../../context/AuthContext';
-// import ProfileAPI, { ProfileResponse } from '../../../../api/api.topicpost.net/profile';
+
+import ProfileAPI, { ProfileResponse } from "@/utils/api/api.topicpost.net/profile";
 
 type EmailPasswordProps = {
   toggle: () => void;
 };
 
 export const EmailPassword: React.FC<EmailPasswordProps> = ({ toggle }) => {
-  // const [modalEmail, setModalEmail] = useState<string>(''); // value属性を型付きのstringで初期化する
-  // const [modalPassword, setModalPassword] = useState<string>(''); // value属性を型付きのstringで初期化する
+  const [modalEmail, setModalEmail] = useState<string>(''); // value属性を型付きのstringで初期化する
+  const [modalPassword, setModalPassword] = useState<string>(''); // value属性を型付きのstringで初期化する
   // const { setLoggedInTrue } = useAuthContext();
-
-  // const toast = new Toast();
-
   // const history = useHistory();
 
-  // const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setModalEmail(event.target.value);
-  // };
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setModalEmail(event.target.value);
+  };
 
-  // const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setModalPassword(event.target.value);
-  // };
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setModalPassword(event.target.value);
+  };
 
-  // // TopicPost にログインするをクリックしたらaxiosを使ってログイン処理を行う
-  // const handleLoginWithPassword = async (event: React.FormEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
-  //   const { data, error } = await supabaseClient.auth.signInWithPassword({
-  //     email: modalEmail,
-  //     password: modalPassword
-  //   });
+  // TopicPost にログインするをクリックしたらaxiosを使ってログイン処理を行う
+  const handleLoginWithPassword = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { data, error } = await supabaseClient.auth.signInWithPassword({
+      email: modalEmail,
+      password: modalPassword
+    });
 
-  //   console.log("data", data);
+    console.log("data", data);
 
-  //   if (error) {
-  //     toast.error(getErrorMessage(error.message));
-  //     return;
-  //   }
+    if (error) {
+      toast.error(getErrorMessage(error.message));
+      return;
+    }
 
-  //   // ログインに成功したらモーダルを閉じる
-  //   toggle();
+    // ログインに成功したらモーダルを閉じる
+    toggle();
 
-  //   const profile = new ProfileAPI();
-  //   profile.get()
-  //     .then((response: ProfileResponse) => {
-  //       console.log("response", response);
-  //       toast.success('ログインに成功しました');
-  //       setLoggedInTrue();
+    const profile = new ProfileAPI();
+    profile.get()
+      .then((response: ProfileResponse) => {
+        console.log("response", response);
+        toast.success('ログインに成功しました');
+        // setLoggedInTrue();
 
-  //       if (response.data) {
-  //         // 登録済みのProfileがある場合
-  //         history.push("/");
-  //       } else if (response.status === 404) {
-  //         // Profileが登録されていない場合
-  //         history.push(`/profile/edit`);
-  //       } else {
-  //         toast.error('エラーが発生しました');
-  //       }
-  //     })
-  //     .catch((error: any) => {
-  //       console.error(error);
-  //       toast.error('ログインに失敗しました');
-  //     });
-  // };
+        if (response.data) {
+          // 登録済みのProfileがある場合
+          // history.push("/");
+        } else if (response.status === 404) {
+          // Profileが登録されていない場合
+          // history.push(`/profile/edit`);
+        } else {
+          toast.error('エラーが発生しました');
+        }
+      })
+      .catch((error: any) => {
+        console.error(error);
+        toast.error('ログインに失敗しました');
+      });
+  };
 
   return (
-    <form className="space-y-6" /*onSubmit={handleLoginWithPassword}*/>
+    <form className="space-y-6" onSubmit={handleLoginWithPassword}>
       <div>
         <Label htmlFor="modalEmail">メールアドレス</Label>
         <Text
           type="email"
           id="modalEmail"
-          // value={modalEmail}
-          // onChange={handleEmailChange}
+          value={modalEmail}
+          onChange={handleEmailChange}
           placeholder="example@topicpost.net"
           required={true}
         />
@@ -94,12 +92,13 @@ export const EmailPassword: React.FC<EmailPasswordProps> = ({ toggle }) => {
         <Text
           type="password"
           id="modalPassword"
-          // value={modalPassword}
-          // onChange={handlePasswordChange}
+          value={modalPassword}
+          onChange={handlePasswordChange}
           placeholder="••••••••"
           required={true}
         />
       </div>
+      {/* Cookieを使うようになってから追加修正をする */}
       {/* <div className="flex justify-between">
         <div className="flex items-start">
           <div className="flex items-center h-5">
