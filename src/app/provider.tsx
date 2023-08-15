b43e70f37e98fbd712cd7935c4cdeef59a50295d
chihiros <szrkw2ui2a@gmail.com>
2023-08-16 02:56:09 +0900

    add: Providerの更新

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,13 +1,19 @@
 'use client';
 import React from 'react';
 import { LoginModalProvider } from '@/components/organisms/Modal/Modal.Provider';
+import { AuthContextProvider } from '@/components/organisms/context/Auth.Provider';
+import { ProfileDataContextProvider } from '@/components/organisms/context/ProfileData.Provider';
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <LoginModalProvider>
-        {children}
-      </LoginModalProvider>
+      <ProfileDataContextProvider>
+        <AuthContextProvider>
+          <LoginModalProvider>
+            {children}
+          </LoginModalProvider>
+        </AuthContextProvider>
+      </ProfileDataContextProvider>
     </>
   )
 }
