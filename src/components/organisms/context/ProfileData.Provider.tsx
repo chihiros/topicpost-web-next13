47bac9f53e80403a3d848fba93494431daab9950
chihiros <szrkw2ui2a@gmail.com>
2023-08-28 00:01:47 +0900

    add: use clientを追加

--- src/components/organisms/context/ProfileData.Provider.tsx
+++ src/components/organisms/context/ProfileData.Provider.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { createContext, useState, useContext } from 'react';
 import ProfileAPI, { ProfileData, ProfileResponse } from '@/utils/api/api.topicpost.net/profile';
 
