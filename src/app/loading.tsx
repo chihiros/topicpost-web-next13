44750bff4bccd04ac42cc82f4076e1175ee58a2e
chihiros <szrkw2ui2a@gmail.com>
2023-09-16 13:23:21 +0900

    fix: ローティング画面の変更

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,24 @@
-export default function Loading() {
-  return <p className="p-2">Now on Loading...</p>;
+// export default function Loading() {
+//   return <p className="p-2">Now on Loading...</p>;
+// }
+
+const Loading: React.FC = () => {
+  return (
+    <div style={{
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: 'rgba(220, 220, 220, 0.7)',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      zIndex: 1000
+    }}>
+      <p>Loading...</p>
+    </div>
+  );
 }
+
+export default Loading;
