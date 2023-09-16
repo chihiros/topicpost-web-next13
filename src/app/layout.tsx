599510f3e41837f26b1e961b8b7126a06dc0953e
chihiros <szrkw2ui2a@gmail.com>
2023-09-17 02:48:42 +0900

    fix: Twitterの設定を変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,14 @@ export const metadata: Metadata = {
       },
     ],
   },
-}
+  twitter: {
+    card: 'summary_large_image',
+    title: siteName,
+    description: description,
+    site: '@com_index',
+    creator: '@com_index',
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
