8b27e8b85fe02d020436a4b0f0ad5ba7c4969af5
chihiros <szrkw2ui2a@gmail.com>
2023-09-18 14:14:20 +0900

    add: カードの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import { Button, SubmitButton } from '@/components/atoms/Button';
 // import ArrowRight from '@/components/atoms/Icon/Arrow';
 import { Text, Textarea, Annotation } from '@/components/atoms/Input';
-import { Card } from '@/components/atoms/Card';
+import Card from '@/components/atoms/Card';
 import Breadcrumb, { BreadcrumbProps } from '@/components/molecules/Breadcrumb/Breadcrumb';
 
 export default function Home() {
@@ -24,15 +24,19 @@ export default function Home() {
         className='w-96'
       />
       <Annotation>aaaa</Annotation> */}
-      {/* <Card
-        title='タイトル'
-        date="2021/10/10"
-        content='コンテンツ'
-        recreationId='1'
-        data='data'
-      /> */}
       <Breadcrumb
         breadcrumb={breadcrumb}
+      />
+      <Card
+        title='タイトル'
+        created_at="2023-05-21T17:46:45.313871Z"
+        href='/recreation/1'
+      />
+
+      <Card
+        title='タイトル'
+        created_at="2023-05-21T17:46:45.400000Z"
+        href='/recreation/2'
       />
     </>
   )
